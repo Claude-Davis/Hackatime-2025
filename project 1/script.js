6072a781914c254adf9252f4205fcf6591813912
Claude Davis <claudedavis2024@gmail.com>
2025-06-22 19:54:53 -0400

    project 1: "duck dogs" video game started

---
+++ project 1/script.js
@@ -0,0 +1,90 @@
+window.addEventListener("load", function() {
+    const canvas = this.document.getElementById("canvas0");
+    const ctx = canvas.getContext("2d");
+    canvas.width = 500;
+    canvas.height = 500;
+})
+
+class Layer {
+    constructor(game){
+        //
+    }
+    update(){
+        //
+    }
+    draw(context){
+        //
+    }
+}
+
+class Background {
+    constructor(game){
+        //
+    }
+    update(){
+        //
+    }
+    draw(context){
+        //
+    }
+}
+
+class InputHandler{
+    constructor(game){
+        //
+    }
+    update(){
+        //
+    }
+    draw(context){
+        //
+    }
+}
+
+class UI{
+    constructor(game){
+        //
+    }
+    update(){
+        //
+    }
+    draw(context){
+        //
+    }
+}
+
+class Player {
+    constructor(game){
+        //
+    }
+    update(){
+        //
+    }
+    draw(context){
+        //
+    }
+}
+
+class Fowl{
+    constructor(game){
+        //
+    }
+    update(){
+        //
+    }
+    draw(context){
+        //
+    }
+}
+
+class Hunter{
+    constructor(game){
+        //
+    }
+    update(){
+        //
+    }
+    draw(context){
+        //
+    }
+}
