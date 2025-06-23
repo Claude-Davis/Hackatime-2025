c57e3fc0c2f32e18104e93498221e80f1955065a
Claude Davis <claudedavis2024@gmail.com>
2025-06-23 13:00:13 -0400

    javascript updated, notes for game added

--- project 1/script.js
+++ project 1/script.js
@@ -5,6 +5,29 @@ window.addEventListener("load", function() {
     canvas.height = 500;
 })
 
+
+class InputHandler{
+    constructor(game){
+        this.game = game;
+        window.addEventListener('keydown', e=> {
+            if (( (e.key === 'ArrowUp') ||
+                  (e.key === 'ArrowDown') ||
+                  (e.key === 'ArrowRight') ||
+                  (e.key === 'ArrowLeft') ||
+                  (e.key === 'W') ||
+                  (e.key === 'S') ||
+                  (e.key === 'D') ||
+                  (e.key === 'A')
+                ) && thid.game.keys.indexOf(e.key) === -1) this.game.keys.push(e.key);  //the key is pushed into the array 'keys' (Game constructor)
+        });
+        window.addEventListener('keyup', e => {
+            if (this.game.keys.indexOf(e.key) > -1 ) {
+                this.game.keys.splice(this.game.keys.indexOf(e.key), 1);
+            }
+        })
+    }
+}
+
 class Layer {
     constructor(game){
         //
@@ -18,18 +41,6 @@ class Layer {
 }
 
 class Background {
-    constructor(game){
-        //
-    }
-    update(){
-        //
-    }
-    draw(context){
-        //
-    }
-}
-
-class InputHandler{
     constructor(game){
         //
     }
@@ -55,13 +66,30 @@ class UI{
 
 class Player {
     constructor(game){
-        //
+        this.game = game;
+        this.width = 80;
+        this.height = 80;
+        this.x = 20;
+        this.y = 20;
+        this.speedX = 0;
+        this.speedY = 0;
     }
     update(){
-        //
+        //y-axis
+        if (this.game.keys.includes('ArrowUp') || (this.game.keys.includes('W'))) this.speedY = -2;
+        else if (this.game.keys.includes('ArrowDown') || (this.game.keys.includes('S'))) this.speedY = 2;
+        else this.speedY = 0; 
+        this.y += this.speedY;
+
+            //x-axis
+        if (this.game.keys.includes('ArrowRight') || (this.game.keys.includes('D'))) this.speedX = 2;
+        else if (this.game.keys.includes('ArrowLeft') || (this.game.keys.includes('A'))) this.speedX = -2;
+        else this.speedX = 0;
+        this.x += this.speedX;
     }
     draw(context){
-        //
+        context.fillStyle = '#0000ff';
+        context.fillRect(this.x, this.y, this.width, this.height);
     }
 }
 
